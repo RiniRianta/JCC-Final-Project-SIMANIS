290aed80a1be557311559b33e97e859b815475bb
dkdev <andikaaddobe@outlook.com>
2022-03-30 23:20:08 +0700

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+@extends('template.main')
+@section('content')
+<div class="row bg-light rounded mx-0">
+    <div class="">
+        <article class="ms-3 mt-3">
+            <h3>SIMANIS</h3>
+            <p>SIMANIS (Sistem Manajemen Informasi Sekolah) merupakan aplikasi yang dibuat untuk memenuhi kebutuhan manajemen sekolah. Didalam aplikasi ini terdapat beberapa fitur di antaranya:</p>
+            <ul>
+                <li>Fitur Absensi Siswa</li>
+                <li>Fitur Nilai Siswa</li>
+                <li>Fitur Data Siswa</li>
+                <li>Fitur Data Guru</li>
+                <li>Fitur Jadwal Pelajaran</li>
+            </ul>
+        </article>
+    </div>
+</div>
+@endsection
