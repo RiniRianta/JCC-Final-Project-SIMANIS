bc9a63ea72a81ccd7b85721d028ce467adf5da1f
dikuyyy <andikaaddobe@outlook.com>
2022-04-01 00:40:03 +0700

    Tambah controller guru

---
+++ resources/views/guru/index.blade.php
@@ -0,0 +1,18 @@
+@extends('template.main')
+@section('content')
+<div class="row bg-light rounded mx-0">
+    <div class="">
+        <article class="ms-3 mt-3">
+            <h3>SIMANIS</h3>
+            <p>Halo, selamat datang di halaman guru. Terdapat beberapa fitur dihalaman ini, diantaranya</p>
+            <ul>
+                <li>Fitur Data Guru</li>
+                <li>Fitur Data Siswa</li>
+                <li>Fitur Agenda Mengajar</li>
+                <li>Fitur Data Absensi Siswa</li>
+                <li>Fitur Nilai Siswa</li>
+            </ul>
+        </article>
+    </div>
+</div>
+@endsection
