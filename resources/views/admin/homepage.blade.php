f31f1eb295a421afb4fb7aa44cbe7a9663a4ce9a
hanhanhanafi <me.hanhanhanafi@gmail.com>
2022-04-03 00:19:52 +0700

    mengganti icon pada admin homepage

--- resources/views/admin/homepage.blade.php
+++ resources/views/admin/homepage.blade.php
@@ -3,7 +3,7 @@
     <div class="row g-2">
         <div class="col-sm-6 col-xl-6">
             <div class="bg-light rounded d-flex align-items-center justify-content-between p-4" style="height:200px">
-                <i class="fa fa-chart-line fa-3x text-primary"></i>
+                <i class="fa fa-users fa-3x text-primary"></i>
                 <div class="ms-3">
                     <p class="mb-2">Jumlah Siswa</p>
                     <h2 class="mb-0 text-center">{{$tsiswa}}</h2>
@@ -12,7 +12,7 @@
         </div>
         <div class="col-sm-6 col-xl-6">
             <div class="bg-light rounded d-flex align-items-center justify-content-between p-4" style="height:200px">
-                <i class="fa fa-chart-bar fa-3x text-primary"></i>
+                <i class="fa fa-users-cog fa-3x text-primary"></i>
                 <div class="ms-3">
                     <p class="mb-2">Jumlah Guru</p>
                     <h2 class="mb-0 text-center">{{$tguru}}</h2>
@@ -21,7 +21,7 @@
         </div>
         <div class="col-sm-6 col-xl-6">
             <div class="bg-light rounded d-flex align-items-center justify-content-between p-4" style="height:200px">
-                <i class="fa fa-chart-area fa-3x text-primary"></i>
+                <i class="fa fa-home fa-3x text-primary"></i>
                 <div class="ms-3">
                     <p class="mb-2">Jumlah Kelas</p>
                     <h2 class="mb-0 text-center">{{$tkelas}}</h2>
